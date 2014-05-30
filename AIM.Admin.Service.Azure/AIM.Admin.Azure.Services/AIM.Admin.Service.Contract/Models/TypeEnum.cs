c0ee66d4805a9c0091f7cdfece0e2e224cce2e53
Chase Marcum <chase.m.marcum@gmail.com>
2014-05-30 14:51:47 -0700

    refresh

---
+++ AIM.Admin.Service.Azure/AIM.Admin.Azure.Services/AIM.Admin.Service.Contract/Models/TypeEnum.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.Serialization;
+
+namespace AIM.Service.Client.Models
+{
+    [DataContract(Name = "QuestionType")]
+    public enum TypeEnum : int
+    {
+        [EnumMember]
+        MultipleChoice = 0,
+
+        [EnumMember]
+        AllThatApply = 1,
+
+        [EnumMember]
+        FreeForm = 2
+    }
+}
