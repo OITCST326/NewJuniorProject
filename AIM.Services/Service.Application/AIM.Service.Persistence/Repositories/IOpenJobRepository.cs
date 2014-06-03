6bfaf0be2f8c01f03eae68d34cecc06bee6ff893
Chase Marcum <chase.m.marcum@gmail.com>
2014-06-03 16:56:10 -0700

    Uploading Application Service

---
+++ AIM.Services/Service.Application/AIM.Service.Persistence/Repositories/IOpenJobRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TrackableEntities.Patterns;
+using AIM.Service.Entities.Models;
+
+namespace AIM.Service.Persistence.Repositories
+{
+    public interface IOpenJobRepository : IRepository<OpenJob>, IRepositoryAsync<OpenJob>
+    {
+        Task<IEnumerable<OpenJob>> GetOpenJobs();
+        Task<OpenJob> GetOpenJob(int id);
+        Task<bool> DeleteOpenJob(int id);
+    }
+}
---
+++ AIM.Services/Service.Application/AIM.Service.Persistence/Repositories/IQuestionRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using TrackableEntities.Patterns;
+using AIM.Service.Entities.Models;
+
+namespace AIM.Service.Persistence.Repositories
+{
+    public interface IQuestionRepository : IRepository<Question>, IRepositoryAsync<Question>
+    {
+        Task<IEnumerable<Question>> GetQuestions();
+        Task<Question> GetQuestion(int id);
+        Task<bool> DeleteQuestion(int id);
+    }
+}
