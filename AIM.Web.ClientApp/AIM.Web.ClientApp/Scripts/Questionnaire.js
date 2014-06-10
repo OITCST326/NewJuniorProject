1bd79b104db3a53c0e1b3747713d610dd02b4988
jordan-ringo <jordan.ringo@oit.edu>
2014-06-10 07:34:27 -0700

    more updates to the questionnaire views

--- AIM.Web.ClientApp/AIM.Web.ClientApp/Scripts/Questionnaire.js
+++ AIM.Web.ClientApp/AIM.Web.ClientApp/Scripts/Questionnaire.js
@@ -1,4 +1,6 @@
-﻿$(function () {
+﻿
+
+$(function () {
     $("#form-3").steps({
         titleTemplate: "#title#",
         bodyTag: "fieldset",
@@ -8,10 +10,19 @@
                 return true;
             }
 
-            // Forbid suppressing "Warning" step if the user is to young
-            if (newIndex === 3 && Number($("#age").val()) < 18) {
-                return false;
-            }
+            ////////// Forbid suppressing "Warning" step if the user is to young
+            ////////if (newIndex === 3 && Number($("#age").val()) < 18) {
+            ////////    return false;
+            ////////}
+
+            //if (newIndex === 2 && Request.Form["question1"] == null) {
+            //    return false;
+            //}
+
+            //if (Request.Form["@currentQuestion"] != null)
+            //{
+            //    selectedAnswers.Add(Request.Form["@currentQuestion"].ToString());
+            //}
 
             var form = $(this);
 
@@ -29,15 +40,15 @@
             return form.valid();
         },
         onStepChanged: function (event, currentIndex, priorIndex) {
-            // Suppress (skip) "Warning" step if the user is old enough.
-            if (currentIndex === 2 && Number($("#age").val()) >= 18) {
-                $(this).steps("next");
-            }
+            ////////// Suppress (skip) "Warning" step if the user is old enough.
+            ////////if (currentIndex === 2 && Number($("#age").val()) >= 18) {
+            ////////    $(this).steps("next");
+            ////////}
 
-            // Suppress (skip) "Warning" step if the user is old enough and wants to the previous step.
-            if (currentIndex === 2 && priorIndex === 3) {
-                $(this).steps("previous");
-            }
+            ////////// Suppress (skip) "Warning" step if the user is old enough and wants to the previous step.
+            ////////if (currentIndex === 2 && priorIndex === 3) {
+            ////////    $(this).steps("previous");
+            ////////}
         },
         onFinishing: function (event, currentIndex) {
             var form = $(this);
@@ -50,10 +61,32 @@
             return form.valid();
         },
         onFinished: function (event, currentIndex) {
-            var form = $(this);
+            var form  = $(this);
 
-            // Submit form input
-            form.submit();
+            var passed = true;
+
+            $("input[type=radio]:checked").each(function () {
+                if( this.value == "false" )
+                {
+                    passed = false;
+                }
+            });
+            $("input[type=checkbox]:checked").each(function () {
+                if (this.value == "false") {
+                    passed = false;
+                }
+            });
+
+            if (passed == true)
+            {
+                form.submit(window.location.href = '/OpenJob/Index/');
+            }
+            else
+            {
+                // Submit form input
+                form.submit();
+            }
+            
         }
     }).validate({
         errorPlacement: function (error, element) {
@@ -65,4 +98,23 @@
             }
         }
     });
+
+    function foo() {
+        //var url = '@Url.Action("Index", "Home")';
+        //window.location.href = url;
+
+        window.location.href = '/OpenJob/Index/';
+    }
+
+    //$('form input[type="radio"]').each(function(){
+    //    // Do your magic here
+    //    if (this.value.match(/\D/)) // regular expression for numbers only.
+    //        Error();
+    //});
+    //$("input[type=radio]:checked").each(function () {
+    //    var t = $(this).val();
+    //});
+
+
+
 });
